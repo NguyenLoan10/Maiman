48a46302d2d4ef6a9820159c1ddf2ad03f439220
Nguyen Loan <Admin@LAPTOP-Q62KFUJF>
2023-05-09 15:56:05 +0700

    edit

---
+++ MMShop/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using MMShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MMShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        QLSSNDKEntities _db = new QLSSNDKEntities();
+        public ActionResult Index()
+        {
+            return View(_db.SanPhams.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MMShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MMShop.MvcApplication" Language="C#" %>
